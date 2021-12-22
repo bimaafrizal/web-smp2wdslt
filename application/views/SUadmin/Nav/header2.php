9731992d2c073db57ac098d471d966222dc915ee
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2021-12-22 16:52:05 +0700

    update logo loading

--- application/views/SUadmin/Nav/header2.php
+++ application/views/SUadmin/Nav/header2.php
@@ -39,7 +39,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="<?= base_url('assets/images/Logo.png') ?>" alt="Logo SMP 2 Wadaslintang" height="60" width="60">
         </div>
 
         <!-- Navbar -->
