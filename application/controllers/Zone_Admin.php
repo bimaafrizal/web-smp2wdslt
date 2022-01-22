e50457be0c0f6206c7ec31b0f4b47fe6db63f61f
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2022-01-22 12:25:42 +0700

    fix pagination guru

--- application/controllers/Zone_Admin.php
+++ application/controllers/Zone_Admin.php
@@ -281,7 +281,7 @@ class Zone_Admin extends CI_Controller
         $config['full_tag_close'] = '</ul></nav></div>';
         $config['num_tag_open'] = '<li class="page-item"><span class="page-link">';
         $config['num_tag_close'] = '</span></li>';
-        $config['cur_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['cur_tag_open'] = '<li class="page-item active"><span class="page-link">';
         $config['cur_tag_close'] = '</span></li>';
         $config['next_tag_open'] = '<li class="page-item"><span class="page-link">';
         $config['next_tagl_close'] = '<span aria-hidden="true">&raquo</span></span></li>';
